d0c09f9bfd4b4a59f0064e7358c3134e09f2b108
rainsunset <15620608789@163.com>
2018-09-11 09:12:13 +0800

    end commit

---
+++ src/main/java/com/seer/panel/config/CorsConfig.java
@@ -0,0 +1,29 @@
+package com.seer.panel.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+import org.springframework.web.filter.CorsFilter;
+
+/**
+ * @autheor ligw
+ * @date 2018/9/4 13:55
+ */
+@Configuration
+public class CorsConfig {
+	private CorsConfiguration buildConfig() {
+		CorsConfiguration corsConfiguration = new CorsConfiguration();
+		corsConfiguration.addAllowedOrigin("*"); // 1
+		corsConfiguration.addAllowedHeader("*"); // 2
+		corsConfiguration.addAllowedMethod("*"); // 3
+		return corsConfiguration;
+	}
+
+	@Bean
+	public CorsFilter corsFilter() {
+		UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+		source.registerCorsConfiguration("/**", buildConfig()); // 4
+		return new CorsFilter(source);
+	}
+}
